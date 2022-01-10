6602bda98a6ac43ebaac9584d29fe6a3c5c31114
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2022-01-10 13:39:30 +0100

    append method

--- js/sight.js
+++ js/sight.js
@@ -11,8 +11,12 @@ class SVGElement {
         for (const [key, value] of Object.entries(attrs)) {
             this.node.setAttributeNS(null, key, value)
          }
-          
-
+        return this
+    }
+    append(element) {
+        const parent = typeof element === 'string'
+          ? document.querySelector(element)
+          : element.node
         return this
     }
     
